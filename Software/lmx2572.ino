c0e6bc43768230fad171e50e0dc35faa74e409b5
Atharva Nandanwar <atharva.nandanwar@colorado.edu>
2020-02-10 18:25:52 -0700

    Added minor functions for lmx2572

---
+++ Software/lmx2572.ino
@@ -0,0 +1,29 @@
+#include "lmx2572.h"
+
+/*
+ * Function for resetting LMX2572
+ */
+void lmx_reset(void)
+{
+}
+
+/*
+ * Function to change frequency to given frequency 
+ */
+void lmx_change_freq(uint32_t frequency)
+{
+}
+
+/*
+ * Function to shift to one of the FSK frequencies
+ */
+void lmx_change_to_fsk_freq(uint8_t dev_sel)
+{
+}
+
+/*
+ * Function for initializing LMX2572
+ */
+void lmx_init(void)
+{
+}
